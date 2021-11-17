e872c732cc77b99bf90affa8375817a9bc0fc698
Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>
2021-11-16 22:34:11 -0800

    update api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "oldprojbe1a78dc": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s38fcde399": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "oldproj": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
