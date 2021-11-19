336750bc185c2b00fe6147ca571b7a2835bc18f0
Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>
2021-11-19 11:28:03 -0800

    api ext

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,12 +16,41 @@ export type AmplifyDependentResourcesAttributes = {
         "s38fcde399": {
             "BucketName": "string",
             "Region": "string"
+        },
+        "dynamo9cb07ea0": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "SortKeyName": "string",
+            "SortKeyType": "string",
+            "Region": "string"
         }
     },
     "api": {
+        "api4d95345d": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        },
         "oldproj": {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "oldproja8355a17": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "oldproj2eb6b2ba": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     }
 }
